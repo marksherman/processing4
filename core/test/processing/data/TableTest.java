f15f6823b6e5bd17ca3d70dc740d11c8c65a1368
eliseferguson <eliseferguson18@gmail.com>
2026-03-11 14:29:27 -0400

    Missing tests to ensure switch cases in Table constructor are changing to specified types

--- core/test/processing/data/TableTest.java
+++ core/test/processing/data/TableTest.java
@@ -1,10 +1,14 @@
 package processing.data;
 
+import org.mockito.*;
+import java.sql.*;
+import java.sql.ResultSet;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class TableTest {
 
@@ -36,4 +40,51 @@ public class TableTest {
         Assert.assertEquals(people[0].name, "Person1");
         Assert.assertEquals(people[0].age, 30);
     }
+
+//    @Before
+//    public void setUp() throws Exception {
+//        Create mock results set to test initialization of Table from a resultSet
+//
+//    }
+
+    @Mock
+    private ResultSet rs;
+    private ResultSetMetaData rsmd;
+
+    @Before
+    public void setUp() throws SQLException {
+        MockitoAnnotations.openMocks(this);
+        when(rs.getMetaData()).thenReturn(rsmd);
+    }
+
+    interface rsRowSetup {
+        void setup(ResultSet rs) throws SQLException;
+    }
+
+    private ResultSet makeMockCols(String colName, int sqlType, rsRowSetup rowSetup) throws SQLException {
+        try {
+            // Creates result set data framework
+            when(rsmd.getColumnCount()).thenReturn(1);
+            when(rsmd.getColumnName(1)).thenReturn(colName);
+            when(rsmd.getColumnType(1)).thenReturn(sqlType);
+
+            // Creates one row with TRUE, stops loop from creating a second with FALSE
+            when(rs.next()).thenReturn(true, false);
+            rowSetup.setup(rs);
+
+            return rs;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void varcharIsString() throws SQLException {
+        ResultSet mockRS = makeMockCols("name", Types.VARCHAR, r -> when(r.getString(1)).thenReturn("hello"));
+
+        Table t = new Table(mockRS);
+
+        assertEquals(Table.STRING, t.getColumnType(0));
+    }
+
 }
